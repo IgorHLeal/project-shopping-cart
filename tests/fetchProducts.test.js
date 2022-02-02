4082cba03e1f7d7a2bcaf75311bc85292b8748a7
Igor Leal <igorhenriqueleal3@gmail.com>
2022-02-02 19:24:25 -0300

    Descrição dos testes do requisito 8 modificadas.

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,20 +4,20 @@ const { results } = require('../mocks/search');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  it('Verifica se a função fetchProducts é uma função', () => {
+  it('Teste se fetchProducts é uma função', () => {
     expect.assertions(1);
 
     expect(typeof fetchProducts).toBe('function');
   });
 
-  it('Verifica se ao chamar a função fetchProducts com o argumento \'computador\' a função fetch é chamada', async() => {
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async() => {
     expect.assertions(1);
 
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   });
 
-  it('Verifica se ao chamar a função fetchProducts com o argumento \'computador\', a função fetch utiliza o endpoint \'https://api.mercadolibre.com/sites/MLB/search?q=computadorq\'', async () => {
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
   expect.assertions(1);
 
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
@@ -25,14 +25,14 @@ describe('1 - Teste a função fecthProducts', () => {
   expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  it('Verifica se o retorno da função fetchProducts com o argumento \'computador\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
     expect.assertions(1);
 
     const result = await fetchProducts('computador');
     expect(result).toEqual(computadorSearch);
   });
 
-  it('Verifica se  ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
     expect.assertions(1);
 
     try {
