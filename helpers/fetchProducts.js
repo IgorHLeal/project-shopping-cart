422e2fec5db07865beda2df27d4da6e941384a7f
Igor Leal <igorhenriqueleal3@gmail.com>
2022-02-02 22:48:31 -0300

    Comentários e referências adicionados.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,9 +4,14 @@ const fetchProducts = async (item) => {
   if (!item) {
     throw new Error('You must provide an url');
   }
+  // Função refatorada e concluída com ajuda da Luá - Turma 19 - Tribo A
+  // try {
   const response = await fetch(url);
   const data = await response.json();
   return data;
+  /*  } catch (error) {
+    return error;
+   } */
 };
 
 if (typeof module !== 'undefined') {
